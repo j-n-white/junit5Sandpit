363aee30371dab5a80847169be63d54c319c2cdd
James White <jwhite@scottlogic.co.uk>
2017-09-26 11:26:56 +0100

    Added basic example

---
+++ junit4and5/src/test/java/com/scottlogic/RomanNumeralConverterJunit4Test.java
@@ -0,0 +1,128 @@
+package com.scottlogic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class RomanNumeralConverterJunit4Test {
+
+    private RomanNumeralConverter testee;
+
+    @Before
+    public void setUp() {
+        testee = new RomanNumeralConverter();
+    }
+
+    @Test
+    public void oneReturnsI() {
+        assertEquals("I", testee.convertToRomanNumerals(1));
+    }
+
+    @Test
+    public void twoReturnsII() {
+        assertEquals("II", testee.convertToRomanNumerals(2));
+    }
+
+    @Test
+    public void threeReturnsIII() {
+        assertEquals("III", testee.convertToRomanNumerals(3));
+    }
+
+    @Test
+    public void fourReturnsIV() {
+        assertEquals("IV", testee.convertToRomanNumerals(4));
+    }
+
+    @Test
+    public void fiveReturnsV() {
+        assertEquals("V", testee.convertToRomanNumerals(5));
+    }
+
+    @Test
+    public void sixReturnsVI() {
+        assertEquals("VI", testee.convertToRomanNumerals(6));
+    }
+
+    @Test
+    public void sevenReturnsVII() {
+        assertEquals("VII", testee.convertToRomanNumerals(7));
+    }
+
+    @Test
+    public void eightReturnsVII() {
+        assertEquals("VIII", testee.convertToRomanNumerals(8));
+    }
+
+    @Test
+    public void nineReturnsIX() {
+        assertEquals("IX", testee.convertToRomanNumerals(9));
+    }
+
+    @Test
+    public void tenReturnsX() {
+        assertEquals("X", testee.convertToRomanNumerals(10));
+    }
+
+    @Test
+    public void fourtyReturnsXL() {
+        assertEquals("XL", testee.convertToRomanNumerals(40));
+    }
+
+    @Test
+    public void fiftyReturnsL() {
+        assertEquals("L", testee.convertToRomanNumerals(50));
+    }
+
+    @Test
+    public void ninetyReturnsXC() {
+        assertEquals("XC", testee.convertToRomanNumerals(90));
+    }
+
+    @Test
+    public void oneHundredReturnsC() {
+        assertEquals("C", testee.convertToRomanNumerals(100));
+    }
+
+    @Test
+    public void fourHundredReturnsCD() {
+        assertEquals("CD", testee.convertToRomanNumerals(400));
+    }
+
+    @Test
+    public void fiveHundredReturnsD() {
+        assertEquals("D", testee.convertToRomanNumerals(500));
+    }
+
+    @Test
+    public void nineHundredReturnsCM() {
+        assertEquals("CM", testee.convertToRomanNumerals(900));
+    }
+
+    @Test
+    public void oneThousandReturnsM() {
+        assertEquals("M", testee.convertToRomanNumerals(1000));
+    }
+
+    @Test
+    public void oneThousandNineHundredAndFifyFourReturnsMCMLIV() {
+        assertEquals("MCMLIV", testee.convertToRomanNumerals(1954));
+    }
+
+    @Test
+    public void zeroReturnsNulla() {
+        assertEquals("nulla", testee.convertToRomanNumerals(0));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void negativeThrowsException() {
+        try {
+            testee.convertToRomanNumerals(-1);
+            fail("Exception should have been thrown.");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Negative numbers are not supported", e.getMessage());
+            throw e;
+        }
+    }
+}
---
+++ junit4and5/src/test/java/com/scottlogic/RomanNumeralConverterJunit5Test.java
@@ -0,0 +1,125 @@
+package com.scottlogic;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class RomanNumeralConverterJunit5Test {
+
+    private static RomanNumeralConverter testee;
+
+    @BeforeAll
+    static void setUp() {
+        testee = new RomanNumeralConverter();
+    }
+
+    @Test
+    void oneReturnsI() {
+        assertEquals("I", testee.convertToRomanNumerals(1));
+    }
+
+    @Test
+    void twoReturnsII() {
+        assertEquals("II", testee.convertToRomanNumerals(2));
+    }
+
+    @Test
+    void threeReturnsIII() {
+        assertEquals("III", testee.convertToRomanNumerals(3));
+    }
+
+    @Test
+    void fourReturnsIV() {
+        assertEquals("IV", testee.convertToRomanNumerals(4));
+    }
+
+    @Test
+    void fiveReturnsV() {
+        assertEquals("V", testee.convertToRomanNumerals(5));
+    }
+
+    @Test
+    void sixReturnsVI() {
+        assertEquals("VI", testee.convertToRomanNumerals(6));
+    }
+
+    @Test
+    void sevenReturnsVII() {
+        assertEquals("VII", testee.convertToRomanNumerals(7));
+    }
+
+    @Test
+    void eightReturnsVII() {
+        assertEquals("VIII", testee.convertToRomanNumerals(8));
+    }
+
+    @Test
+    void nineReturnsIX() {
+        assertEquals("IX", testee.convertToRomanNumerals(9));
+    }
+
+    @Test
+    void tenReturnsX() {
+        assertEquals("X", testee.convertToRomanNumerals(10));
+    }
+
+    @Test
+    void fourtyReturnsXL() {
+        assertEquals("XL", testee.convertToRomanNumerals(40));
+    }
+
+    @Test
+    void fiftyReturnsL() {
+        assertEquals("L", testee.convertToRomanNumerals(50));
+    }
+
+    @Test
+    void ninetyReturnsXC() {
+        assertEquals("XC", testee.convertToRomanNumerals(90));
+    }
+
+    @Test
+    void oneHundredReturnsC() {
+        assertEquals("C", testee.convertToRomanNumerals(100));
+    }
+
+    @Test
+    void fourHundredReturnsCD() {
+        assertEquals("CD", testee.convertToRomanNumerals(400));
+    }
+
+    @Test
+    void fiveHundredReturnsD() {
+        assertEquals("D", testee.convertToRomanNumerals(500));
+    }
+
+    @Test
+    void nineHundredReturnsCM() {
+        assertEquals("CM", testee.convertToRomanNumerals(900));
+    }
+
+    @Test
+    void oneThousandReturnsM() {
+        assertEquals("M", testee.convertToRomanNumerals(1000));
+    }
+
+    @Test
+    void oneThousandNineHundredAndFifyFourReturnsMCMLIV() {
+        assertEquals("MCMLIV", testee.convertToRomanNumerals(1954));
+    }
+
+    @Test
+    void zeroReturnsNulla() {
+        assertEquals("nulla", testee.convertToRomanNumerals(0));
+    }
+
+    @Test
+    void negativeThrowsException() {
+        IllegalArgumentException actual = assertThrows(IllegalArgumentException.class, () -> testee.convertToRomanNumerals(-1));
+        assertEquals("Negative numbers are not supported", actual.getMessage());
+    }
+}
